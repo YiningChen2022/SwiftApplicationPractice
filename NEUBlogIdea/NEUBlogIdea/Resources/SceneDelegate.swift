175c5439879ae987c8c159a0ab45c15a9cad6ecf
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-10-04 20:12:15 -0400

    add Manager and debug

--- NEUBlogIdea/NEUBlogIdea/Resources/SceneDelegate.swift
+++ NEUBlogIdea/NEUBlogIdea/Resources/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            let  vc=ViewController();
+            let  vc=HomeViewController();
             window.rootViewController=vc;
             self.window=window
             
