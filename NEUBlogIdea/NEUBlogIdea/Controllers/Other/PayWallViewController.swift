a30e7b832f44a7a64a37afd76a292cc59f1ef314
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-10-06 13:39:33 -0400

    subscribtion page

--- NEUBlogIdea/NEUBlogIdea/Controllers/Other/PayWallViewController.swift
+++ NEUBlogIdea/NEUBlogIdea/Controllers/Other/PayWallViewController.swift
@@ -13,14 +13,47 @@ class PayWallViewController: UITabBarController {
     //Header image
 
     //pricing and product info
+    private let heroView=PayWallDescriptionView()
+    
     //CTA Button
+    private let buyButton: UIButton = {
+        let button = UIButton();
+        button.setTitle(("Subscribe"), for: .normal)
+        button.backgroundColor = .systemPink
+        button.setTitleColor(.white, for: .normal)
+        button.layer.masksToBounds = true
+        return button
+    }()
+    private let restoreButton: UIButton = {
+        let button = UIButton();
+        button.setTitle(("Restore Purchases"), for: .normal)
+        button.setTitleColor(.link, for: .normal)
+        button.layer.masksToBounds = true
+        return button
+    }()
     //Term of services
+    private let termsView: UITextView = {
+        let textView = UITextView()
+        textView.isEditable = false
+        textView.textAlignment = .center
+        textView.textColor = .secondaryLabel
+        textView.font = .systemFont(ofSize: 14)
+        textView.text = "This is an auto-renewable subscription. It will be charged to your iTunes account before each pay period. You can cancel anytime by going into your Settings>subscriptions. Restore purchases if previous subscripted"
+        return textView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "NEU Plus Premium"
         view.backgroundColor = .systemBackground
         view.addSubview(header)
+        view.addSubview(buyButton)
+        view.addSubview(restoreButton)
+        view.addSubview(termsView)
+        view.addSubview(heroView)
         setUpCloseButton()
+
+        setUpButtons()
         
     }
     override func viewDidLayoutSubviews() {
@@ -30,6 +63,39 @@ class PayWallViewController: UITabBarController {
                               width: view.width,
                               height: view.height/3.2
         )
+        termsView.frame = CGRect(x:10,
+                                 y:view.height-100,
+                                 width: view.width-20,
+                                 height: 100
+           )
+        restoreButton.frame=CGRect(x:25,
+                                  y:termsView.top-70,
+                                  width: view.width-50,
+                                  height: 50
+            )
+        buyButton.frame=CGRect(x:25,y:restoreButton.top-60,
+                                width: view.width-50,
+                                height: 50
+            )
+        heroView.frame=CGRect(x:0,y:header.bottom,
+                                width: view.width,
+                              height: buyButton.top-view.safeAreaInsets.top-header.height
+            )
+        
+    }
+    
+    
+    private func setUpButtons(){
+        buyButton.addTarget(self,action: #selector (didTapSubscribe),for: .touchUpInside)
+        restoreButton.addTarget(self,action: #selector (didTaprestore),for: .touchUpInside)
+        
+            
+    }
+    @objc private func didTapSubscribe(){
+        
+    }
+    @objc private func didTaprestore(){
+        
     }
     private func setUpCloseButton(){
         navigationItem.rightBarButtonItem = UIBarButtonItem(
