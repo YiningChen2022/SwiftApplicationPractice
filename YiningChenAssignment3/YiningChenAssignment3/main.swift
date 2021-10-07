0911172aa45957fecb70b193aa98480843610bb2
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-10-07 13:07:46 -0400

    updates of assignment 3

--- YiningChenAssignment3/YiningChenAssignment3/main.swift
+++ YiningChenAssignment3/YiningChenAssignment3/main.swift
@@ -34,12 +34,85 @@ print(getProteinIntake(weight:150))
 func getBodyFatPercentage (weight: Int, height: Int, Age: Int, Gender:String)->Double{
     var result=0.0
     if Gender == "Man"{
-         result = getBMI(weight: weight, height: height)*1.2/0.23*Double (Age)-16.2
+         result = getBMI(weight: weight, height: height)*1.2+0.23*Double (Age)-16.2
     }else{
-        result = getBMI(weight: weight, height: height)*1.2/0.23*Double (Age)-5.4
+        result = getBMI(weight: weight, height: height)*1.2+0.23*Double (Age)-5.4
 
     }
       return result
 }
-
 print(getBodyFatPercentage(weight: 50,height: 165,Age: 16, Gender: "Women"))
+
+
+//Exercise: Swift Structures
+// Create a structure ‘Health’ with four properties: Height(Double), Weight (Double), Gender(String) and Age (Int)
+struct Health{
+    var weight=0.0
+    var height=0.0
+    var gender=""
+    var  Age=0
+}
+// Create 2 instances of the structure providing the height, weight , gender and age.
+let someHealth = Health(weight:50.5,height:166.5,gender:"Woman",Age:20)
+let otherHealth = Health(weight:100.2,height:188,gender:"Man",Age:50)
+
+
+
+//Modify the function ‘getBMI’ to accept two Doubles perform the same operation
+func getBMI (weight: Double, height: Double)->Double{
+    let BMI=(weight/height/height)*10000
+    return BMI
+}
+//Modify the function called ‘getBodyFatPercentage’  which will take four parameters  weight , height in Double, Age in Int and Gender in String.
+func getBodyFatPercentage (weight: Double, height: Double, Age: Int, Gender:String)->Double{
+    var result=0.0
+    if Gender == "Man"{
+         result = getBMI(weight: weight, height: height)*1.2+0.23*Double (Age)-16.2
+    }else{
+        result = getBMI(weight: weight, height: height)*1.2+0.23*Double (Age)-5.4
+
+    }
+      return result
+}
+//Pass the height, weight , gender and age from the 2 instances of ‘Health’ that you just created to the ‘getBodyFatPercentage’ function and print the output
+
+
+print(getBodyFatPercentage(weight: someHealth.weight,height:someHealth.height,Age: someHealth.Age, Gender: someHealth.gender))
+print(getBodyFatPercentage(weight: otherHealth.weight,height:otherHealth.height,Age: otherHealth.Age, Gender: otherHealth.gender))
+
+
+
+//Exercise: Swift Classes
+//Create a class ‘ClubMember’ with an empty function ‘memberType()’.
+class ClubMember{
+    func memberType(){}
+}
+
+//Create a class ‘Employee’ as a subclass of ‘ClubMember’ override the function ‘memberType()’ that prints ‘I am an Employee’.
+class Employee: ClubMember{
+    override func memberType(){
+        print("I am an Employee")
+    }
+}
+//Create a class ‘Customer’ as a subclass of ‘ClubMember’ override the function ‘memberType’ that prints ‘I am a Customer’.
+class Customer: ClubMember{
+    override func memberType(){
+        print("I am an Customer")
+    }
+}
+//Create an instance of class ‘Employee’ and class ‘Customer’.
+let employee = Employee()
+let customer = Customer()
+
+//Print the memberType from each of these instances.
+employee.memberType()
+customer.memberType()
+//Print if each of these instances is an instance of class ‘ClubMember’.
+print(employee is ClubMember)
+print(customer is ClubMember)
+
+
+//Exercise: Swift OptionalsAsk the user their name, take an input using command line.
+//If no input was provided print ‘Welcome Anonymous User’
+//If the input is all numbers print ‘Welcome Code Name ’ followed by the number
+//Otherwise print ‘Welcome ’ followed by the String entered
