52890a78c35a9c99c579574751c4d7e2fc6eae06
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-11-02 18:33:45 -0400

    updates

---
+++ Assignment7InterfaceBuilder/EmptyApp/Manage Customer/ManageCustomerViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ManageCustomerViewController.swift
+//  EmptyApp
+//
+//  Created by Yining Chen on 11/2/21.
+//  Copyright © 2021 rab. All rights reserved.
+//
+
+import UIKit
+
+class ManageCustomerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func CloseWindow(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+ 
+    
+    
+    @IBAction func DidTapAddCustomer(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+
+}
