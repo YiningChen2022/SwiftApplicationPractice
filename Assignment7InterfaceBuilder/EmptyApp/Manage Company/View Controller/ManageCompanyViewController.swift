76621f5f6ab77d99d87618312b1f84dc434ae41c
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-11-02 18:53:07 -0400

    updates

---
+++ Assignment7InterfaceBuilder/EmptyApp/Manage Company/View Controller/ManageCompanyViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ManageCompanyViewController.swift
+//  EmptyApp
+//
+//  Created by Yining Chen on 11/2/21.
+//  Copyright © 2021 rab. All rights reserved.
+//
+
+import UIKit
+
+class ManageCompanyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func closeWindow(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Assignment7InterfaceBuilder/EmptyApp/Manage Stock/View Controller/ManageStockViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ManageStockViewController.swift
+//  EmptyApp
+//
+//  Created by Yining Chen on 11/2/21.
+//  Copyright © 2021 rab. All rights reserved.
+//
+
+import UIKit
+
+class ManageStockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func CloseWindow(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
