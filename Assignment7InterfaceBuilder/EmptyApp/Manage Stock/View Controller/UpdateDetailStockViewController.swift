ea6f35599bbe50b0d1f01a35e05820bce131dc16
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-11-03 21:13:16 -0400

    updates

--- Assignment7InterfaceBuilder/EmptyApp/Manage Stock/View Controller/UpdateDetailStockViewController.swift
+++ Assignment7InterfaceBuilder/EmptyApp/Manage Stock/View Controller/UpdateDetailStockViewController.swift
@@ -8,12 +8,25 @@
 
 import UIKit
 
-class UpdateDetailStockViewController: UIViewController {
+class UpdateDetailStockViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var lasttradefield: UITextField!
+    
+    
+    @IBOutlet weak var Ratingfield: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func didName(_ sender: UITextField) {
+        sender.resignFirstResponder()
     }
     
     @IBAction func closewindow(_ sender: UIButton) {
