f98af32b19213525b0926e7ad9a6abcde3231b9b
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-11-04 17:19:07 -0400

    updates

--- Assignment7InterfaceBuilder/EmptyApp/Manage Stock/SearchViewController/SearchByCompanyViewController.swift
+++ Assignment7InterfaceBuilder/EmptyApp/Manage Stock/SearchViewController/SearchByCompanyViewController.swift
@@ -8,23 +8,46 @@
 
 import UIKit
 
-class SearchByCompanyViewController: UIViewController {
+class SearchByCompanyViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.register(UITableViewCell.self,
+                                 forCellReuseIdentifier: "AnimalCell")
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //Table View
+    func tableView(_ tableView: UITableView,numberOfRowsInSection section:Int)->Int{
+        
+        return (AppDelegate.GlobalVariable.companylist.testCompanylist.getsize())
+                   
+      }
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)->UITableViewCell{
+      
+          let cell = UITableViewCell(style:UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
+       cell.textLabel?.text = AppDelegate.GlobalVariable.companylist.testCompanylist.toString()[indexPath.row]
+          return (cell)
+      }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
+        let selected = AppDelegate.GlobalVariable.companylist.testCompanylist.toString()[indexPath.row]
+        let id=Int(selected.split(separator: " ")[0])!
+        AppDelegate.GlobalVariable.selectedCompanytoSearch=id
+        DispatchQueue.main.async {
+            let vc = CompanySelectedViewController()
+                self.present(vc, animated: true, completion: nil)
+        }
     }
-    */
+    
+   
+ 
+       
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func closeWindow(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 
 }
--- Assignment7InterfaceBuilder/EmptyApp/Manage Stock/SearchViewController/SearchByNameViewController.swift
+++ Assignment7InterfaceBuilder/EmptyApp/Manage Stock/SearchViewController/SearchByNameViewController.swift
@@ -16,15 +16,8 @@ class SearchByNameViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func CloseWindow(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
 }
