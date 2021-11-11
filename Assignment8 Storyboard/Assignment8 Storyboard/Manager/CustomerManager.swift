fc1efdcbe4e5dabac0c99ca40d4d16bb48276298
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-11-11 14:20:09 -0500

    updates

---
+++ Assignment8 Storyboard/Assignment8 Storyboard/Manager/CustomerManager.swift
@@ -0,0 +1,12 @@
+//
+//  CustomerManager.swift
+//  Assignment8 Storyboard
+//
+//  Created by Yining Chen on 11/11/21.
+//
+
+import Foundation
+final class CustomerManager{
+    public let testcustomerlist = CustomerDirecrtory(customerList: Array())
+    
+}
---
+++ Assignment8 Storyboard/Assignment8 Storyboard/Manager/OrderManager.swift
@@ -0,0 +1,13 @@
+//
+//  OrderManager.swift
+//  Assignment8 Storyboard
+//
+//  Created by Yining Chen on 11/11/21.
+//
+
+import Foundation
+
+final class OrderManager{
+    public let testOrderlist = OrderDirecrtory(orderList: Array())
+    
+}
