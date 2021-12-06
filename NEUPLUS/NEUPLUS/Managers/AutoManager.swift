c701876688d6fd6950f11d41408e8c95b72a5cbb
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-12-06 11:47:53 -0500

    upload

---
+++ NEUPLUS/NEUPLUS/Managers/AutoManager.swift
@@ -0,0 +1,8 @@
+//
+//  AutoManager.swift
+//  NEUPLUS
+//
+//  Created by Yining Chen on 12/6/21.
+//
+
+import Foundation
---
+++ NEUPLUS/NEUPLUS/Managers/DatabaseManager.swift
@@ -0,0 +1,8 @@
+//
+//  DatabaseManager.swift
+//  NEUPLUS
+//
+//  Created by Yining Chen on 12/6/21.
+//
+
+import Foundation
