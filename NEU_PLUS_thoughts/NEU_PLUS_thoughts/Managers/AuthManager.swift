84ba8aa924cd21c23d0f2d5c6bf2d55b28d57196
YiningChen001586646 <chen.yinin@northeastern.edu>
2021-12-07 17:19:40 -0500

    auth

--- NEU_PLUS_thoughts/NEU_PLUS_thoughts/Managers/AuthManager.swift
+++ NEU_PLUS_thoughts/NEU_PLUS_thoughts/Managers/AuthManager.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+final class AuthManager{
+    static let shared=AuthManager()
+    
+    private let auth = Auth.auth()
+     
+    private init(){}
+    
+    //sign in
+    
+    //sign up
+    
+    //sign out
+    
+    //if signed in
+          public var isSignedIn: Bool{
+        return auth.currentUser != nil
+    }
+    public func signUp(email: String,
+                       password: String,
+                       completion:@escaping (Bool)->Void){
+        
+    }
+         
+    
+}
--- NEU_PLUS_thoughts/NEU_PLUS_thoughts/Managers/DatebaseManager.swift
+++ NEU_PLUS_thoughts/NEU_PLUS_thoughts/Managers/DatebaseManager.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+import FirebaseFirestore
+final class DatabaseManager{
+    static let shared=DatabaseManager()
+    
+    private let database=Firestore.firestore()
+    private init(){}
+}
